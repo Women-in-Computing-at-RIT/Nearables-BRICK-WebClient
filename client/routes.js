66871853454dd674b92461a8d3e501a0f8780b6e
Matthew Crocco <matthewcrocco@gmail.com>
2017-04-30 02:32:33 -0400

    feat(routes): Re-implement routes with AuthRoute and new pages
    Signed-off-by: Matthew Crocco <matthewcrocco@gmail.com>

--- client/routes.js
+++ client/routes.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 
+import QRCodePage from './containers/pages/QRCodePage';
 import Landing from './containers/pages/Landing';
 import UserPage from './containers/pages/UserPage';
+import { NotFoundError, InDevPage } from './containers/pages/ErrorPage';
+import { AuthRoute } from './lib/MoreRoutes';
 
 export const makeRoutes = () => (
     <Switch>
-      <Route exact path='/' component={Landing}/>
-      <Route path='/u' component={UserPage} />
+      <Route exact path="/" component={Landing}/>
+      
+      <AuthRoute exact path="/qrcode" component={QRCodePage} />
+      <Route exact path="/qrcode/:id" component={QRCodePage} />
+      
+      <AuthRoute exact path="/u" component={UserPage} />
+      <AuthRoute exact path="/u/event" component={InDevPage} />
+      <Route component={NotFoundError} />
     </Switch>
 );
 
